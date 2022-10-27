f1344c040a63d4fb4fb3cb3437837eeeb7c72dd2
Sylvain Puccinelli <sylvain@mbp-de-sylvain-1.home>
2022-10-27 11:43:18 +0200

    start working on pagination

--- __tests__/data/datasources/heraultdata.datasource.test.ts
+++ __tests__/data/datasources/heraultdata.datasource.test.ts
@@ -3,7 +3,7 @@ import HeraultdataDatasource from "../../../src/data/datasources/heraultdata.dat
 import PlaygroundHeraultDataModel from "../../../src/data/models/playground_heraultdata.model";
 
 describe("heraultdataDatasource", () => {
-  const expectedURL =
+  const baseUrl =
     "https://www.herault-data.fr/api/records/1.0/search/?dataset=aires-de-jeux-herault";
   let datasource: HeraultdataDatasource;
 
@@ -11,16 +11,26 @@ describe("heraultdataDatasource", () => {
     datasource = new HeraultdataDatasource();
   });
 
-  const mockFetchRequest = (expectedResult: any, status: number) => {
-    globalThis.fetch = jest.fn().mockResolvedValue({
-      status,
-      json: jest.fn().mockResolvedValue(expectedResult),
-    } as unknown as Response);
+  const mockFetchRequest = (
+    mockImplementation: { expectedResult: any; expectedStatus: number }[]
+  ) => {
+    let mockFetch: jest.Mock = jest.fn();
+    mockImplementation.forEach((mock) => {
+      mockFetch = mockFetch.mockResolvedValueOnce({
+        status: mock.expectedStatus,
+        json: jest.fn().mockResolvedValue(mock.expectedResult),
+      });
+    });
+
+    globalThis.fetch = mockFetch;
   };
 
   it("fetchAll must call global fetch method with spécific url", async () => {
     expect.assertions(1);
-    mockFetchRequest({}, 200);
+    const expectedURL = `${baseUrl}?rows=25&start=0`;
+    mockFetchRequest([
+      { expectedResult: { records: [] }, expectedStatus: 200 },
+    ]);
 
     await datasource.fetchAllPlaygrounds();
     expect(globalThis.fetch).toHaveBeenCalledWith(expectedURL, {
@@ -30,21 +40,51 @@ describe("heraultdataDatasource", () => {
 
   it("fetchAll must return PlaygroundHeraultDatamodel array depending on fetch result", async () => {
     expect.assertions(2);
-    const expectedItemsCount = 10;
-    const expectedResult = require("../../mocks/heraultdata_fetchall_page1.json");
-    mockFetchRequest(expectedResult, 200);
+    const expectedItemsCount = 24;
+    const expectedResult = require("../../mocks/heraultdata_fetchall_page1_last.json");
+    mockFetchRequest([{ expectedResult, expectedStatus: 200 }]);
     const result: PlaygroundHeraultDataModel[] =
       await datasource.fetchAllPlaygrounds();
     expect(result).toBeDefined();
     expect(result.length).toBe(expectedItemsCount);
   });
 
-  it("fetchAll must throw an error if fetch request return status code other than 200", async () => {
+  it.skip("fetchAll must throw an error if fetch request return status code other than 200", async () => {
     expect.assertions(1);
     const expectedError = { error: "internal server error", code: 500 };
-    mockFetchRequest(expectedError, 500);
+    mockFetchRequest([{ expectedResult: expectedError, expectedStatus: 500 }]);
     await expect(datasource.fetchAllPlaygrounds()).rejects.toStrictEqual(
       new Error(JSON.stringify(expectedError))
+    );
+  });
+
+  it("fetchAll must call global fetch function until all results are returned", async () => {
+    expect.assertions(3);
+    const expectedURL = `${baseUrl}?rows=25`;
+    const expectedRequestInit = { method: "GET" };
+    const expectedResult1 = require("../../mocks/herault_data_fetch_pagination/heraultdata_fetchall_page1.json");
+    const expectedResult2 = require("../../mocks/herault_data_fetch_pagination/heraultdata_fetchall_page2.json");
+    const expectedResult3 = require("../../mocks/herault_data_fetch_pagination/heraultdata_fetchall_page3.json");
+    mockFetchRequest([
+      { expectedResult: expectedResult1, expectedStatus: 200 },
+      { expectedResult: expectedResult2, expectedStatus: 200 },
+      { expectedResult: expectedResult3, expectedStatus: 200 },
+    ]);
+    await datasource.fetchAllPlaygrounds();
+    expect(globalThis.fetch).toHaveBeenNthCalledWith(
+      1,
+      `${expectedURL}&start=0`,
+      expectedRequestInit
+    );
+    expect(globalThis.fetch).toHaveBeenNthCalledWith(
+      2,
+      `${expectedURL}&start=25`,
+      expectedRequestInit
+    );
+    expect(globalThis.fetch).toHaveBeenNthCalledWith(
+      3,
+      `${expectedURL}&start=50`,
+      expectedRequestInit
     );
   });
 });
--- src/data/datasources/heraultdata.datasource.ts
+++ src/data/datasources/heraultdata.datasource.ts
@@ -1,3 +1,12 @@
+import {
+  EMPTY,
+  expand,
+  from,
+  lastValueFrom,
+  Observable,
+  switchMap,
+  tap,
+} from "rxjs";
 import { injectable } from "tsyringe";
 import PlaygroundHeraultDataModel from "../models/playground_heraultdata.model";
 
@@ -5,15 +14,56 @@ import PlaygroundHeraultDataModel from "../models/playground_heraultdata.model";
 export default class HeraultdataDatasource {
   static readonly injectorName: string = "HeraultdataDatasource";
 
-  private readonly url =
+  readonly records_per_page: number = 25;
+
+  private startIndex: number = 0;
+
+  private readonly base_url =
     "https://www.herault-data.fr/api/records/1.0/search/?dataset=aires-de-jeux-herault";
 
   async fetchAllPlaygrounds(): Promise<PlaygroundHeraultDataModel[]> {
-    const serverResponse: Response = await fetch(this.url, { method: "GET" });
-    const serverResponseJson: any = await serverResponse.json();
-    if (serverResponse.status === 200) {
-      return serverResponseJson.records;
-    }
-    throw new Error(JSON.stringify(serverResponseJson));
+    let datas: PlaygroundHeraultDataModel[] = [];
+
+    await lastValueFrom(
+      this.fetchPlaygroundsForStartIndex().pipe(
+        tap((serverResponse: { status: number; response: any }) => {
+          datas = datas.concat(serverResponse.response.records);
+        }),
+        expand((previousData: { status: number; response: any }) =>
+          previousData.status === 200 &&
+          previousData.response.records.length === this.records_per_page
+            ? this.fetchPlaygroundsForStartIndex(
+                this.startIndex + this.records_per_page
+              )
+            : EMPTY
+        )
+      )
+    );
+    return datas;
+    // const serverResponse: { status: number; response: any } =
+    //   await lastValueFrom(this.fetchPlaygroundsForStartIndex());
+    // const serverResponseJson: any = serverResponse.response;
+    // if (serverResponse.status === 200) {
+    //   return serverResponseJson.records;
+    // }
+    // throw new Error(JSON.stringify(serverResponseJson));
+  }
+
+  fetchPlaygroundsForStartIndex(
+    startIndex: number = 0
+  ): Observable<{ status: number; response: any }> {
+    this.startIndex = startIndex;
+    const url = `${this.base_url}?rows=${this.records_per_page}&start=${this.startIndex}`;
+
+    return from(
+      fetch(url, {
+        method: "GET",
+      })
+    ).pipe(
+      switchMap(async (response) => ({
+        status: response.status,
+        response: await response.json(),
+      }))
+    );
   }
 }
